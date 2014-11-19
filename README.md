16263fb0b7b873fbbf2b170c40652722d50a3adf
Michael Burman <miburman@redhat.com>
2014-11-19 16:11:02 +0200

    Add setup.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 rhq-metrics-python-client
 =========================
 
-Python client to access RHQ Metrics
+Python client to access RHQ Metrics. Install by running python setup.py install
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rhq-metrics-python-client',
+      version='0.1',      
+      description='Python client to communicate with RHQ Metrics over HTTP',
+      author='Michael Burman',
+      author_email='miburman@redhat.com',
+      url='http://github.com/burmanm/rhq-metrics-python-client',
+      packages=['rhqmetrics']
+      )
