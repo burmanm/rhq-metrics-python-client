4d0dd6519fe91b3c22fb900a6f8cbe875a337468
Michael Burman <miburman@redhat.com>
2015-02-10 00:23:18 +0200

    Refactor to support newer hawkular-metrics REST-calls. Supports getting metadata and pushing both availability as well as numeric data

--- rhqmetrics/client.py
+++ rhqmetrics/client.py
@@ -3,16 +3,37 @@ import urllib2
 import httplib
 import time
 
+class MetricType:
+    Numeric = 'numeric'
+    Availability = 'availability'
+
+    @staticmethod
+    def short(metric_type):
+        if metric_type is 'numeric':
+            return 'num'
+        else:
+            return 'avail'
+
+class Availability:
+    Down = 'down'
+    Up = 'up'
+
 class RHQMetricsClient:
     """
     Creates new client for RHQ Metrics. If you intend to use the batching feature, remember to call
     flush() on certain intervals to avoid data being stale on the client side for too long.
     """
 
+    """
+    Availability data and numeric data can't be posted in the same call anymore..
+    So they should be separated
+    """
+    
     #last_sent = None
     _batch = []
     
-    def __init__(self, 
+    def __init__(self,
+                 tenant_id,
                  host='localhost',
                  port=8080,
                  batch_size=1):
@@ -27,6 +48,7 @@ class RHQMetricsClient:
 
         http://{host}:{port}/rhq-metrics/
         """
+        self.tenant_id = tenant_id
         self.host = host
         self.port = port
         self.batch_size = batch_size
@@ -36,12 +58,20 @@ class RHQMetricsClient:
     """
 
     def _get_url(self, service):
-        return "http://{0}:{1}/rhq-metrics/{2}".format(self.host, str(self.port), service)
+        return "http://{0}:{1}/rhq-metrics/{2}/{3}".format(self.host, str(self.port), self.tenant_id, service)
 
+    def _get_metrics_url(self, metric_type):
+        return self._get_url('metrics') + "/{0}".format(metric_type)
+
+    def _get_metrics_data_url(self, metric_type):
+        return self._get_metrics_url(metric_type) + '/data'
+    
     def _time_millis(self):
         return int(round(time.time() * 1000))
 
     def _post(self, url, json_data):
+        print url
+        
         try:
             req = urllib2.Request(url=url, data=json_data)
             req.add_header('Content-Type', 'application/json')
@@ -56,11 +86,27 @@ class RHQMetricsClient:
         except urllib2.URLError, e:
             print "Error, could not send event to RHQ Metrics: " + str(e.reason)
 
+    def _get(self, url):
+        try:
+            req = urllib2.Request(url=url)
+            req.add_header('Content-Type', 'application/json')
+            res = urllib2.urlopen(req)        
+            data = json.load(res)
+
+            res.close()
+            return data
+        
+        except urllib2.HTTPError, e:
+            print "Error, RHQ Metrics responded with http code: " + str(e.code)
+
+        except urllib2.URLError, e:
+            print "Error, could not send event to RHQ Metrics: " + str(e.reason)
+
             
     """
     External methods
     """
-    def put(self, data):
+    def put(self, metric_type, metric_id, data):
         """
         Send datapoint(s) to the server.
 
@@ -68,38 +114,81 @@ class RHQMetricsClient:
         of such dicts
         """
         if isinstance(data, list):
-            self._batch.extend(data)
+            batch = data
+            # self._batch.extend(data)
         else:
-            self._batch.append(data)
+            batch = []
+            batch.append(data)
+            # self._batch.append(data)
 
-        if len(self._batch) >= self.batch_size:
-            self.flush()
+        # if len(self._batch) >= self.batch_size:
+        #     self.flush()
+
+        post_dict = [{ 'name': metric_id, 'data': batch }]        
+        json_data = json.dumps(post_dict, indent=2)
+        self._post(self._get_metrics_data_url(metric_type), json_data)
         """
         Allow setting maximum interval between flushes:
           - set up a timer task that does self._flush() after X seconds
             - recreate new one
           - If a batch size is met, reset that timer
         """
-        
-    def create(self, id, value, timestamp=None):
+
+    def push(self, metric_id, value, timestamp=None):
         """
-        Creates new datapoint and sends to the server.
+        Creates new datapoint and sends to the server. Value should be
+        type of "Availability" or "up", "down" for availability metrics and otherwise
+        a float type of for numerical values.
         """
         if timestamp is None:
             timestamp = self._time_millis()
 
-        item = { 'id': id,
-                 'timestamp': timestamp,
-                 'value': float(value)}
+        if self._isfloat(value):
+            metric_type = MetricType.Numeric
+        else:
+            metric_type = MetricType.Availability
+            
+        # only str ("up", "down" and floats are allowed, rest should be converted)
+        # if isinstance(value, Availability):
+        #     value = str(value)
+        # elif isinstance(value, int):
+        #     value = float(value)
+        # elif isinstance(value, long):
+        #     value = float(value)
+            
+        item = { 'timestamp': timestamp,
+                 'value': value
+        }
 
-        self.put(item)
+        self.put(metric_type, metric_id, item)
 
     def flush(self):
         """
         Flushes the internal batch queue to the server, regardless if the size limit has
         been reached.
+
+        Numerics and availability data.. uh
+        """
         """
         if len(self._batch) > 0:
             json_data = json.dumps(self._batch, indent=2)
-            self._post(self._get_url('metrics'), json_data)
+            self._post(self._get_metrics(MetricType.Numeric), json_data)
             self._batch = []
+        """
+        
+    def query_metadata(self, query_type):
+        """
+        Query available metric metadata. Use 'avail' or 'num' or MetricType.Availability / MetricType.Numeric
+        """
+        if isinstance(query_type, MetricType):
+            query_type = MetricType.short(query_type)
+            
+        metadata_url = self._get_url('metrics') + '?type=' + MetricType.short(query_type)
+        return self._get(metadata_url)
+    
+    def _isfloat(self, value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
---
+++ rhqmetrics/client_test.py
@@ -0,0 +1,56 @@
+import unittest
+#import client
+from client import *
+#from client import RHQMetricsClient
+
+class TestMetricFunctionsBase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = RHQMetricsClient(tenant_id=1)
+
+
+class TenantTestCase(TestMetricFunctionsBase):
+
+    # def runTest(self):
+        # Create tenant
+        # Fetch tenants and check that the new created one is there..
+        # pass
+
+    def test_tenant_creation(self):
+        # Create tenant
+        # Check that tenant was created with correct values
+        pass    
+
+class MetricsTestCase(TestMetricFunctionsBase):
+
+    # def runTest(self):
+    #     # Create numeric metrics
+    #     # Read numeric metrics
+    #     # Create availability metric
+    #     # Read availability metric
+    #     #self.test_numeric()
+    #     self.test_availability()
+    #     pass
+
+    def test_numeric(self):
+        self.client.push('test.numeric', float(4.35))
+        # Assert that query_metadata returns info about num id = 1
+        # meta = self.client.query_metadata(MetricType.Numeric)
+        # Assert that value is 4.35
+        pass
+
+    def test_availability(self):
+        self.client.push('test.avail.1', Availability.Up)
+        self.client.push('test.avail.2', 'down')
+        meta = self.client.query_metadata(MetricType.Availability)
+        # Assert that we have two of them, and one is availability and one is numeric
+        # with correct names..
+        pass        
+
+    def test_multi(self):
+        # Insert multiple availability & numeric in the same call
+        # Test fetching of the data
+        pass
+        
+if __name__ == '__main__':
+    unittest.main()
